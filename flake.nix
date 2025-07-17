5287b7634f1b533b16abf2e28d48f5a9d50e99c9
Glauber S Santana <glauber.silva14@gmail.com>
2025-07-16 21:30:47 -0300

    feat: add initial project configuration files

---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "PDFGen with typst because LaTeX is too much for me ";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [ "aarch64-linux" "x86_64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs systems;
+      # TODO: I think we can move this to callPackage
+      mgPackage = { pkgs }: pkgs.writeShellScriptBin "mg" ''
+        if [ -z "$1" ]; then
+          echo "Error: Please provide a migration name"
+          echo "Usage: mg <migration_name>"
+          exit 1
+        fi
+
+        ${pkgs.go-migrate}/bin/migrate create -ext sql -dir ./internal/db/migrations -tz UTC "$1"
+      '';
+    in
+    {
+      packages = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "pdfgen";
+            version = "alpha";
+            src = ./.;
+            vendorHash = "sha256-s1LsDR/Q30lZObKOldxFf627GNHzG2pYJGrAQsCxAeQ=";
+            meta = with pkgs.lib; {
+              description = "Library / API for generating PDF documents using typst";
+              homepage = "https://github.com/glwbr/pdfgen";
+              license = licenses.mit;
+              mainProgram = "pdfgen";
+            };
+          };
+
+          mg = mgPackage { inherit pkgs; };
+        }
+      );
+
+      devShells = forEachSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_24
+              yarn
+              go go-migrate treefmt typst sqlc go-swag air
+              self.packages.${system}.mg
+            ];
+            shellHook = ''
+              export DATABASE_URL=postgres://admin:admin@localhost:5432/pdfgen?sslmode=disable
+              export PORT=8081
+              export CGO_ENABLED=0
+              echo "Run 'air' for auto-recompilation"
+              echo "Run 'mg <name>' to create migrations"
+              echo "Run 'sqlc generate' to create the needed queries and models"
+              '';
+          };
+        }
+      );
+
+      apps = forEachSystem (system: {
+        default = {
+          type = "app";
+          program = "${nixpkgs.legacyPackages.${system}.lib.getExe self.packages.${system}.default}";
+        };
+      });
+
+      defaultPackage = forEachSystem (system: self.packages.${system}.default);
+      defaultApp = forEachSystem (system: self.apps.${system}.default);
+    };
+}
